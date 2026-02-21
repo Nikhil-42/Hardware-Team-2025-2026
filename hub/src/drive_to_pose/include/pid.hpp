d053091222c6d52bc2a7acface814570fd2dff7e
Nikhil Iyer <iyer.h.nikhil@gmail.com>
2026-02-21 05:29:36 -0500

    Added action server and PID controller. Need to write action client to complete DriveToPose

---
+++ hub/src/drive_to_pose/include/pid.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include <cmath>
+
+class PID
+{
+	public:
+		PID(double kp, double ki, double kd, double max_output) :
+		kp_(kp), ki_(ki), kd_(kd), max_output_(max_output) {}
+
+		float compute(double error, double dt)
+		{
+
+			integrator_ += error * dt;
+			double derivative = (error - prev_error_)/dt;
+
+			double output = kp_ * error + ki_ * integrator_ + kd_ * derivative;
+			prev_error_ = error;
+
+			// clamp output (may need to clamp integator later)
+			if(std::abs(output) > max_output)
+			{
+				output = (output < 0) ? -max_output : max_output;
+			}
+			return output;
+		}
+
+		void reset()
+		{
+			integrator_ = 0.0;
+			prev_error = 0.0;
+		}
+
+	private:
+		double kp_;
+		double ki_;
+		double kd_;
+
+		double max_output_;
+		double integrator_;
+		double prev_error_;
+}
