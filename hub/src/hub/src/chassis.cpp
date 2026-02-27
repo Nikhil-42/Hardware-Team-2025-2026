b12f1cc80fbe34fb94c9bf072639d3c6782d2b72
Nikhil-42 <iyer.nikhil@ufl.edu>
2026-02-27 01:34:24 -0500

    added xor checksum to remove possible corrupted frames and some failed turtlesim action attempts

--- hub/src/hub/src/chassis.cpp
+++ hub/src/hub/src/chassis.cpp
@@ -8,12 +8,50 @@
 #include <errno.h>
 #include <termios.h>
 #include <unistd.h>
+#include <array>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 
 using namespace std::chrono_literals;
+
+std::array<double, 36> get_pose_covariance()
+{
+	//create row major covariance matrix for pose
+	// may need to tune these values
+	std::array<double, 36>pose_cov{};
+	pose_cov.fill(0.0);
+
+	pose_cov[0] = 0.05; //cov(x)
+	pose_cov[7] = 0.05; //cov(y)
+	pose_cov[35] = 0.1; //cov(yaw)
+
+	return pose_cov;
+}
+
+std::array<double, 36> get_twist_covariance()
+{
+	std::array<double, 36>twist_cov{};
+	twist_cov.fill(0.0);
+
+	twist_cov[0] = 0.1; //cov(vx)
+	twist_cov[7] = 0.1; //cov(vy)
+	twist_cov[35] = 0.2; //cov(wz)
+
+	return twist_cov;
+
+}
+
+uint8_t xor_checksum(uint8_t* data, const uint32_t len)
+{
+	uint8_t checksum = 0;
+	for(uint32_t i = 0; i < len; i++)
+	{
+		checksum ^= data[i];
+	}
+	return checksum;
+}
 
 int read_blocking(int fd, void* buf, size_t nbytes) {
   size_t idx = 0;
@@ -40,6 +78,9 @@ class Chassis : public rclcpp::Node
     {
       this->declare_parameter("device", "/dev/ttyAMA0");
       std::string device = this->get_parameter("device").as_string();
+
+      pose_cov_ = get_pose_covariance();
+      twist_cov_ = get_twist_covariance();
 
       RCLCPP_INFO(this->get_logger(), "Attempting to open serial port: %s", device.c_str());
       this->serial_port_ = open(device.c_str(), O_RDWR | O_NOCTTY);
@@ -75,14 +116,18 @@ class Chassis : public rclcpp::Node
         "cmd_vel", 10,
         [this](const geometry_msgs::msg::Twist::SharedPtr msg) {
           // Serialize and send the command over UART
-          char buffer[2 + 4 * 3 + 2];
+          char buffer[2 + 4 * 3 + 1 + 2];
           buffer[0] = 0xFF;
           buffer[1] = 0xFF;
           *(float*)(buffer+2) = static_cast<float>(msg->linear.x);
           *(float*)(buffer+6) = static_cast<float>(msg->linear.y);
           *(float*)(buffer+10) = static_cast<float>(msg->angular.z);
-          buffer[14] = 0xEE;
+          uint8_t data[4*3];
+          const uint32_t DATA_LEN = sizeof(data)/sizeof(data[0]);
+          memcpy(&data, &buffer[2], DATA_LEN);
+          buffer[14] = (char)xor_checksum(data, DATA_LEN);
           buffer[15] = 0xEE;
+          buffer[16] = 0xEE;
 
           ssize_t n = write(this->serial_port_, buffer, sizeof(buffer));
           if (n < 0)
@@ -118,10 +163,10 @@ class Chassis : public rclcpp::Node
           }
 
           RCLCPP_INFO(this->get_logger(), "Start word found!");
-          
+
           while (rclcpp::ok()) {
             // Read payload + tail
-            char buf[3*4 + 3*4 + 2];
+            char buf[3*4 + 3*4 + 1 + 2];
             const uint32_t LEN = sizeof(buf) / sizeof(buf[0]);
 
             ssize_t n = read_blocking(this->serial_port_, buf, sizeof(buf));
@@ -136,11 +181,22 @@ class Chassis : public rclcpp::Node
               break;
             }
 
+			uint8_t recieved_checksum = buf[LEN - 3];
+			uint8_t data[2 * 3 * 4];
+			const uint32_t DATA_LEN = sizeof(data)/sizeof(data[0]);
+			memcpy(&data, &buf, DATA_LEN);
+			uint8_t calculated_checksum = xor_checksum(data, DATA_LEN);
+			if(recieved_checksum != calculated_checksum)
+			{
+				RCLCPP_ERROR(this->get_logger(), "Corrupted packet (checksum mismatch)");
+				break;
+			}
+
             // RCLCPP_INFO(this->get_logger(), "Before: ");
             // for (size_t i = 0; i+3 < LEN; i += 4) {
             //   RCLCPP_INFO(this->get_logger(), "%02x%02x%02x%02x", buf[i], buf[i+1], buf[i+2], buf[i+3]);
             // }
-            
+
             for (size_t i = 0; i < 6; ++i) {
               char flip[4];
               for (size_t j = 0; j < 4; ++j) {
@@ -169,10 +225,12 @@ class Chassis : public rclcpp::Node
 
             msg.pose.pose.orientation.z = sinf32(odom.posetheta / 2.0f);
             msg.pose.pose.orientation.w = cosf32(odom.posetheta / 2.0f);
+            msg.pose.covariance = pose_cov_;
 
             msg.twist.twist.linear.x = odom.velx;
             msg.twist.twist.linear.y = odom.vely;
             msg.twist.twist.angular.z = odom.veltheta;
+            msg.twist.covariance = twist_cov_;
 
             this->odom_pub->publish(msg);
 
@@ -199,6 +257,8 @@ class Chassis : public rclcpp::Node
     rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub;
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub;
     std::thread read_thread_;
+	std::array<double, 36> pose_cov_;
+	std::array<double, 36> twist_cov_;
 };
 
 int main(int argc, char ** argv)
